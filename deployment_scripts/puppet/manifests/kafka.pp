edbe3a5ca9bfbc7a0136e98e9f9a906662767d54
Volodymyr Kornylyuk <vkornylyuk@mirantis.com>
2016-07-26 11:39:24 +0300

    Initial commit
    Change-Id: Icc80ec5baba9086b0277e394fe8d8e42e2b27dc0

---
+++ deployment_scripts/puppet/manifests/kafka.pp
@@ -0,0 +1,59 @@
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+notice('fuel-plugin-kafka: kafka.pp')
+
+$deployment_id           = hiera('deployment_id')
+$master_ip               = hiera('master_ip')
+$uid                     = hiera('kafka::uid')
+$heap_size               = hiera('kafka::jvm_heap_size')
+$num_partitions          = hiera('kafka::num_partitions')
+$replication_factor      = hiera('kafka::replication_factor')
+$log_retention_hours     = hiera('kafka::log_retention_hours')
+$kafka                   = hiera_hash('kafka')
+$plugin_version          = $kafka['metadata']['plugin_version']
+$array_version           = split($plugin_version, '[.]')
+$major_version           = "${$array_version[0]}.${$array_version[1]}"
+
+$kafka_version = '0.10.0.0'
+
+$mirror_url = "http://${master_ip}:8080/plugins/kafka-${major_version}/repositories/ubuntu"
+$log_options = '-Dlog4j.configuration=file:/opt/kafka/config/log4j.properties -Dkafka.logs.dir=/var/log/kafka'
+
+class { 'kafka':
+  version => $kafka_version,
+  scala_version => '2.11',
+  mirror_url => $mirror_url,
+}
+
+class { 'kafka::broker':
+  config => {
+    'broker.id' => $uid,
+    'zookeeper.connect' => 'localhost:2181',
+    'inter.broker.protocol.version' => $kafka_version,
+    'num.partitions' => $num_partitions,
+    'replication.factor' => $replication_factor,
+    'log.retention.hours' => $log_retention_hours,
+  },
+  heap_opts => "-Xmx${heap_size}G -Xms${heap_size}G",
+  log4j_opts => $log_options
+}
+
+file { "/etc/logrotate.d/kafka.conf":
+  ensure  => present,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => template('kafka_zookeeper/kafka_logrotate.conf.erb'),
+}
